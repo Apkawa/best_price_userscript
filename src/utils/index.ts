a141bf95e370f6af2ff38700c4493a29fd215aaf
apkawa <apkawa@gmail.com>
2022-10-23 13:36:18 +0300

    Some refactoring and add ordering feature ozon.ru best_price_calculator.user.js

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -27,3 +27,13 @@ export function mRegExp(regExps: (RegExp | string)[]): RegExp {
       .join(''),
   );
 }
+
+export function round(n: number, parts = 2): number {
+  const i = Math.pow(10, parts);
+  return Math.round(n * i) / i;
+}
+
+export const keys = Object.keys as <T>(o: T) => Extract<keyof T, string>[];
+export const entries = Object.entries as <T>(
+  o: T,
+) => [Extract<keyof T, string>, Exclude<T[keyof T], undefined>][];
