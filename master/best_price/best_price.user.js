0da88f6d5e7059b85ade34a44669e254ea285a68
Apkawa <apkawa@gmail.com>
2022-12-30 09:05:34 +0000

    Deploying to release from @ Apkawa/best_price_userscript@337e5294ec76ecf5d57a32344ca9c4fe64324a74 🚀

--- master/best_price/best_price.user.js
+++ master/best_price/best_price.user.js
@@ -12,12 +12,14 @@
 // @match        https://www.okeydostavka.ru/*
 // @match        https://perekrestok.ru/*
 // @match        https://www.perekrestok.ru/*
+// @match        https://wildberries.ru/*
+// @match        https://www.wildberries.ru/*
 // @homepage     https://github.com/Apkawa/best_price_userscript
 // @homepageUrl  https://github.com/Apkawa/best_price_userscript
 // @supportUrl   https://github.com/Apkawa/best_price_userscript/issues
 // @downloadUrl  https://github.com/Apkawa/best_price_userscript/raw/release/master/best_price/best_price.user.js
 // @updateUrl    https://github.com/Apkawa/best_price_userscript/raw/release/master/best_price/best_price.user.js
-// @version      0.5.4
+// @version      0.5.5
 // ==/UserScript==
 (function() {
     "use strict";
