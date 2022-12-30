943edee52320a3b1adac4b5191c46832fc0f8856
apkawa <apkawa@gmail.com>
2022-12-30 17:42:04 +0300

    build: tune changelog generation
    МЕТА ДАННЫЕ: #6

---
+++ .versionrc.js
@@ -0,0 +1,38 @@
+// https://github.com/conventional-changelog/conventional-changelog-config-spec/blob/master/versions/2.2.0/README.md
+
+module.exports = {
+  'types': [
+    {
+      'type': 'feat',
+      'section': 'Features',
+    },
+    {
+      'type': 'fix',
+      'section': 'Bug Fixes',
+    },
+    {
+      'type': 'chore',
+      'hidden': true,
+    },
+    {
+      'type': 'docs',
+      'hidden': true,
+    },
+    {
+      'type': 'style',
+      'hidden': true,
+    },
+    {
+      'type': 'refactor',
+      'hidden': true,
+    },
+    {
+      'type': 'perf',
+      'hidden': true,
+    },
+    {
+      'type': 'test',
+      'hidden': true,
+    },
+  ],
+};
