b44ef0cab6e92cc4004dc625f4ff77e60b7e6ebf
Apkawa <apkawa@gmail.com>
2023-11-11 15:11:55 +0000

    Deploying to release from @ Apkawa/best_price_userscript@c463276d115b679e07ce39ea6f76e22d1ca70640 🚀

--- master/best_price/best_price.user.js
+++ master/best_price/best_price.user.js
@@ -19,7 +19,7 @@
 // @supportUrl   https://github.com/Apkawa/best_price_userscript/issues
 // @downloadUrl  https://github.com/Apkawa/best_price_userscript/raw/release/master/best_price/best_price.user.js
 // @updateUrl    https://github.com/Apkawa/best_price_userscript/raw/release/master/best_price/best_price.user.js
-// @version      0.5.7
+// @version      0.5.8
 // ==/UserScript==
 (function() {
     "use strict";
@@ -763,11 +763,11 @@
             extra_style: extraStyle,
             force: true
         });
-        const cardList = document.querySelectorAll(".goods-card");
+        const cardList = document.querySelectorAll(".product-card");
         for (const cardEl of cardList) processProductCard(cardEl, {
-            price_sel: ".goods-card__price-now",
-            title_sel: ".goods-card__description",
-            to_render: ".goods-card__price",
+            price_sel: ".price__lower-price",
+            title_sel: ".product-card__name",
+            to_render: ".product-card__price",
             extra_style: extraStyle
         });
     }
@@ -785,15 +785,12 @@
         const cardList = document.querySelectorAll(".product-card > .product-card__wrapper");
         for (const cardEl of cardList) processProductCard(cardEl, {
             price_sel: ".price__lower-price",
-            title_sel: ".goods-name",
+            title_sel: ".product-card__name",
             to_render: ".product-card__price",
-            extra_style: {
-                fontSize: "1rem",
-                color: "black"
-            }
+            extra_style: extraStyle
         });
         const catalogWrapEl = document.querySelector(".product-card-list");
-        const buttonWrapEl = ElementGetOrCreate(document.querySelector(".inner-sorter"), {
+        const buttonWrapEl = ElementGetOrCreate(document.querySelector(".catalog-page__main"), {
             pos: "before"
         });
         if (catalogWrapEl && buttonWrapEl) initReorderCatalog(catalogWrapEl, buttonWrapEl);
