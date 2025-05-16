cd94870753764b52ae1c76dcb7657d9e78174f47
Apkawa <apkawa@gmail.com>
2025-05-16 10:00:14 +0000

    Deploying to release from @ Apkawa/best_price_userscript@b1feeccfd34ebc52e4dfb535da5bcfd7f0754df6 🚀

--- master/best_price/best_price.user.js
+++ master/best_price/best_price.user.js
@@ -161,137 +161,6 @@
     Object.keys;
     const entries = Object.entries;
     const values = Object.values;
-    var __rest = void 0 && (void 0).__rest || function(s, e) {
-        var t = {};
-        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0) t[p] = s[p];
-        if (s != null && typeof Object.getOwnPropertySymbols === "function") {
-            var i = 0;
-            for (p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i])) t[p[i]] = s[p[i]];
-        }
-        return t;
-    };
-    const WORD_BOUNDARY_END = /(?=\s+|[.,);/]|[хx]|[^\u0400-\u04ff]|$)/;
-    const WEIGHT_REGEXP = mRegExp([ /(?<value>\d+[,.]\d+|\d+)/, /\s?/, "(?<unit>", "(?<weight_unit>(?<weight_SI>кг|килограмм(?:ов|а|))|г|грамм(?:ов|а|)|гр)", "|(?<volume_unit>(?<volume_SI>л|литр(?:ов|а|))|мл)", "|(?<length_unit>(?<length_SI>м|метр(?:ов|а|)))", ")\\.?", WORD_BOUNDARY_END ]);
-    function plural(name, plurals = [ "ок", "ки", "ка" ]) {
-        return `${name}(?:${plurals.join("|")})`;
-    }
-    const QUANTITY_UNITS = [ "шт", "рулон", "пакет", "уп", plural("упаков"), plural("салфет"), "таб", "капсул", plural("флакон", [ "", "a", "ов" ]), plural("пар", [ "", "a", "ы" ]) ];
-    const QUANTITY_REGEXP = RegExp(`(?<quantity>\\d+)\\s?(?<quantity_unit>${QUANTITY_UNITS.join("|")})\\.?`);
-    const QUANTITY_2_REGEXP = RegExp(`(?<quantity_2>\\d+)\\s?(?<quantity_2_unit>${QUANTITY_UNITS.join("|")})\\.?`);
-    const COMBINE_DELIMETER_REGEXP = /\s*?(?:[xх*×/]|по)\s*?/;
-    const COMBINE_QUANTITY_LIST = [ mRegExp([ /(?<quantity_2>\d+)/, COMBINE_DELIMETER_REGEXP, QUANTITY_REGEXP ]), mRegExp([ QUANTITY_REGEXP, COMBINE_DELIMETER_REGEXP, /(?<quantity_2>\d+)/ ]), mRegExp([ QUANTITY_2_REGEXP, COMBINE_DELIMETER_REGEXP, QUANTITY_REGEXP ]) ];
-    const COMBINE_QANTITY_WEIGHT_REGEXP_LIST = [ mRegExp([ WEIGHT_REGEXP, COMBINE_DELIMETER_REGEXP, QUANTITY_REGEXP ]), mRegExp([ QUANTITY_REGEXP, COMBINE_DELIMETER_REGEXP, WEIGHT_REGEXP ]), mRegExp([ /(?<quantity>\d+)/, COMBINE_DELIMETER_REGEXP, WEIGHT_REGEXP ]), mRegExp([ WEIGHT_REGEXP, COMBINE_DELIMETER_REGEXP, /(?<quantity>\d+)/ ]) ];
-    function parseGroups(groups, allowSum = true) {
-        const result = {
-            quantity: 1,
-            units: []
-        };
-        if (groups.value) {
-            const valueStr = groups === null || groups === void 0 ? void 0 : groups.value;
-            const unit = groups === null || groups === void 0 ? void 0 : groups.unit;
-            if (valueStr && unit) {
-                let value = parseFloat(valueStr.replace(",", "."));
-                let unit = null;
-                if (groups.weight_unit) {
-                    if (!groups.weight_SI) value /= 1e3;
-                    unit = "кг";
-                }
-                if (groups.volume_unit) {
-                    if (!groups.volume_SI) value /= 1e3;
-                    unit = "л";
-                }
-                if (groups.length_unit) {
-                    if (!groups.length_SI) value /= 1e3;
-                    unit = "м";
-                }
-                if (!unit) throw "Unknown unit";
-                result.units.push({
-                    unit,
-                    value,
-                    total: value
-                });
-            }
-        }
-        if (groups.quantity) {
-            const valueStr = groups === null || groups === void 0 ? void 0 : groups.quantity;
-            if (valueStr) result.quantity = parseInt(valueStr);
-        }
-        if (allowSum && result.quantity > 1) for (const u of result.units) u.total = result.quantity * u.value;
-        return result;
-    }
-    function parseTitle(title) {
-        var _a;
-        for (const r of COMBINE_QANTITY_WEIGHT_REGEXP_LIST) {
-            const rMatch = r.exec(title);
-            if (rMatch) return parseGroups(rMatch.groups);
-        }
-        let groups = {};
-        const weightMatch = WEIGHT_REGEXP.exec(title);
-        if (weightMatch === null || weightMatch === void 0 ? void 0 : weightMatch.groups) groups = weightMatch.groups;
-        let quantity = 0;
-        for (const r of COMBINE_QUANTITY_LIST) {
-            const rMatch = (_a = r.exec(title)) === null || _a === void 0 ? void 0 : _a.groups;
-            if ((rMatch === null || rMatch === void 0 ? void 0 : rMatch.quantity) && (rMatch === null || rMatch === void 0 ? void 0 : rMatch.quantity_2)) {
-                quantity = parseInt(rMatch.quantity) * parseInt(rMatch.quantity_2);
-                break;
-            }
-        }
-        if (quantity) groups.quantity = quantity.toString(); else {
-            const quantityMatch = QUANTITY_REGEXP.exec(title);
-            if (quantityMatch === null || quantityMatch === void 0 ? void 0 : quantityMatch.groups) groups = Object.assign(Object.assign({}, groups), quantityMatch.groups);
-        }
-        let allowSum = true;
-        if (groups === null || groups === void 0 ? void 0 : groups.value) allowSum = false;
-        return parseGroups(groups, allowSum);
-    }
-    function parseTitleWithPrice(title, price) {
-        const _a = parseTitle(title), {units} = _a, titleParsed = __rest(_a, [ "units" ]);
-        const res = Object.assign(Object.assign({}, titleParsed), {
-            units: [],
-            quantity_price: null,
-            quantity_price_display: null
-        });
-        if ((!res.quantity || res.quantity == 1) && !units.length) return null;
-        for (const u of units) {
-            const p = round(price / u.total);
-            res.units.push(Object.assign(Object.assign({}, u), {
-                price: p,
-                price_display: `${p} ₽/${u.unit || "?"}`
-            }));
-        }
-        if (res.quantity > 1) {
-            res.quantity_price = round(price / res.quantity);
-            res.quantity_price_display = `${res.quantity_price} ₽/шт`;
-        }
-        return res;
-    }
-    const BEST_PRICE_CLASS_NAME = "GM-best-price";
-    const BEST_PRICE_WRAP_CLASS_NAME = "GM-best-price-wrap";
-    const ORDER_NAME_LOCAL_STORAGE = "GM-best-price-default-order";
-    const MAX_NUMBER = 99999999999;
-    function renderBestPrice(titleInfo, extraStyle = {}) {
-        const wrapEl = document.createElement("div");
-        wrapEl.className = BEST_PRICE_CLASS_NAME;
-        if (!titleInfo) return wrapEl;
-        for (const u of titleInfo.units) {
-            const el = document.createElement("p");
-            el.innerHTML = u.price_display;
-            wrapEl.appendChild(el);
-        }
-        if (titleInfo.quantity_price_display) {
-            const qtyEl = document.createElement("p");
-            qtyEl.innerHTML = titleInfo.quantity_price_display;
-            wrapEl.appendChild(qtyEl);
-        }
-        if (wrapEl.childNodes.length) {
-            wrapEl.style.border = "1px solid red";
-            wrapEl.style.padding = "5px";
-            wrapEl.style.margin = "5px";
-            wrapEl.style.width = "fit-content";
-            for (const [k, v] of entries(extraStyle || {})) if (typeof v == "string") wrapEl.style[k] = v;
-        }
-        return wrapEl;
-    }
     function byPropertiesOf(sortBy) {
         function compareByProperty(arg) {
             let key;
@@ -319,6 +188,10 @@
     function sort(arr, ...sortBy) {
         arr.sort(byPropertiesOf(sortBy));
     }
+    const BEST_PRICE_CLASS_NAME = "GM-best-price";
+    const BEST_PRICE_WRAP_CLASS_NAME = "GM-best-price-wrap";
+    const ORDER_NAME_LOCAL_STORAGE = "GM-best-price-default-order";
+    const MAX_NUMBER = 99999999999;
     const PREFIX = "bp_";
     function storeParsedTitleToElement(cardEl, parsedTitle) {
         cardEl.classList.add(BEST_PRICE_WRAP_CLASS_NAME);
@@ -425,6 +298,110 @@
             class: BEST_ORDER_BUTTON_CLASS_NAME
         }, ...values(buttons)));
     }
+    var __rest = void 0 && (void 0).__rest || function(s, e) {
+        var t = {};
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0) t[p] = s[p];
+        if (s != null && typeof Object.getOwnPropertySymbols === "function") {
+            var i = 0;
+            for (p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i])) t[p[i]] = s[p[i]];
+        }
+        return t;
+    };
+    const WORD_BOUNDARY_END = /(?=\s+|[.,);/]|[хx]|[^\u0400-\u04ff]|$)/;
+    const WEIGHT_REGEXP = mRegExp([ /(?<value>\d+[,.]\d+|\d+)/, /\s?/, "(?<unit>", "(?<weight_unit>(?<weight_SI>кг|килограмм(?:ов|а|))|г|грамм(?:ов|а|)|гр)", "|(?<volume_unit>(?<volume_SI>л|литр(?:ов|а|))|мл)", "|(?<length_unit>(?<length_SI>м|метр(?:ов|а|)))", ")\\.?", WORD_BOUNDARY_END ]);
+    function plural(name, plurals = [ "ок", "ки", "ка" ]) {
+        return `${name}(?:${plurals.join("|")})`;
+    }
+    const QUANTITY_UNITS = [ "шт", "рулон", "пакет", "уп", plural("упаков"), plural("салфет"), "таб", "капсул", plural("флакон", [ "", "a", "ов" ]), plural("пар", [ "", "a", "ы" ]) ];
+    const QUANTITY_REGEXP = RegExp(`(?<quantity>\\d+)\\s?(?<quantity_unit>${QUANTITY_UNITS.join("|")})\\.?`);
+    const QUANTITY_2_REGEXP = RegExp(`(?<quantity_2>\\d+)\\s?(?<quantity_2_unit>${QUANTITY_UNITS.join("|")})\\.?`);
+    const COMBINE_DELIMETER_REGEXP = /\s*?(?:[xх*×/]|по)\s*?/;
+    const COMBINE_QUANTITY_LIST = [ mRegExp([ /(?<quantity_2>\d+)/, COMBINE_DELIMETER_REGEXP, QUANTITY_REGEXP ]), mRegExp([ QUANTITY_REGEXP, COMBINE_DELIMETER_REGEXP, /(?<quantity_2>\d+)/ ]), mRegExp([ QUANTITY_2_REGEXP, COMBINE_DELIMETER_REGEXP, QUANTITY_REGEXP ]) ];
+    const COMBINE_QANTITY_WEIGHT_REGEXP_LIST = [ mRegExp([ WEIGHT_REGEXP, COMBINE_DELIMETER_REGEXP, QUANTITY_REGEXP ]), mRegExp([ QUANTITY_REGEXP, COMBINE_DELIMETER_REGEXP, WEIGHT_REGEXP ]), mRegExp([ /(?<quantity>\d+)/, COMBINE_DELIMETER_REGEXP, WEIGHT_REGEXP ]), mRegExp([ WEIGHT_REGEXP, COMBINE_DELIMETER_REGEXP, /(?<quantity>\d+)/ ]) ];
+    function parseGroups(groups, allowSum = true) {
+        const result = {
+            quantity: 1,
+            units: []
+        };
+        if (groups.value) {
+            const valueStr = groups === null || groups === void 0 ? void 0 : groups.value;
+            const unit = groups === null || groups === void 0 ? void 0 : groups.unit;
+            if (valueStr && unit) {
+                let value = parseFloat(valueStr.replace(",", "."));
+                let unit = null;
+                if (groups.weight_unit) {
+                    if (!groups.weight_SI) value /= 1e3;
+                    unit = "кг";
+                }
+                if (groups.volume_unit) {
+                    if (!groups.volume_SI) value /= 1e3;
+                    unit = "л";
+                }
+                if (groups.length_unit) {
+                    if (!groups.length_SI) value /= 1e3;
+                    unit = "м";
+                }
+                if (!unit) throw "Unknown unit";
+                result.units.push({
+                    unit,
+                    value,
+                    total: value
+                });
+            }
+        }
+        if (groups.quantity) {
+            const valueStr = groups === null || groups === void 0 ? void 0 : groups.quantity;
+            if (valueStr) result.quantity = parseInt(valueStr);
+        }
+        if (allowSum && result.quantity > 1) for (const u of result.units) u.total = result.quantity * u.value;
+        return result;
+    }
+    function parseTitle(title) {
+        var _a;
+        for (const r of COMBINE_QANTITY_WEIGHT_REGEXP_LIST) {
+            const rMatch = r.exec(title);
+            if (rMatch) return parseGroups(rMatch.groups);
+        }
+        let groups = {};
+        const weightMatch = WEIGHT_REGEXP.exec(title);
+        if (weightMatch === null || weightMatch === void 0 ? void 0 : weightMatch.groups) groups = weightMatch.groups;
+        let quantity = 0;
+        for (const r of COMBINE_QUANTITY_LIST) {
+            const rMatch = (_a = r.exec(title)) === null || _a === void 0 ? void 0 : _a.groups;
+            if ((rMatch === null || rMatch === void 0 ? void 0 : rMatch.quantity) && (rMatch === null || rMatch === void 0 ? void 0 : rMatch.quantity_2)) {
+                quantity = parseInt(rMatch.quantity) * parseInt(rMatch.quantity_2);
+                break;
+            }
+        }
+        if (quantity) groups.quantity = quantity.toString(); else {
+            const quantityMatch = QUANTITY_REGEXP.exec(title);
+            if (quantityMatch === null || quantityMatch === void 0 ? void 0 : quantityMatch.groups) groups = Object.assign(Object.assign({}, groups), quantityMatch.groups);
+        }
+        let allowSum = true;
+        if (groups === null || groups === void 0 ? void 0 : groups.value) allowSum = false;
+        return parseGroups(groups, allowSum);
+    }
+    function parseTitleWithPrice(title, price) {
+        const _a = parseTitle(title), {units} = _a, titleParsed = __rest(_a, [ "units" ]);
+        const res = Object.assign(Object.assign({}, titleParsed), {
+            units: [],
+            quantity_price: null,
+            quantity_price_display: null
+        });
+        if ((!res.quantity || res.quantity == 1) && !units.length) return null;
+        for (const u of units) {
+            const p = round(price / u.total);
+            res.units.push(Object.assign(Object.assign({}, u), {
+                price: p,
+                price_display: `${p} ₽/${u.unit || "?"}`
+            }));
+        }
+        if (res.quantity > 1) {
+            res.quantity_price = round(price / res.quantity);
+            res.quantity_price_display = `${res.quantity_price} ₽/шт`;
+        }
+        return res;
+    }
     function parsePrice(text) {
         var _a, _b;
         text = (_a = text.split("₽")[0]) === null || _a === void 0 ? void 0 : _a.trim();
@@ -443,6 +420,29 @@
     function getPrice(sel, root = document.body) {
         const priceEl = (root || document.body).querySelector(sel);
         return getPriceFromElement(priceEl);
+    }
+    function renderBestPrice(titleInfo, extraStyle = {}) {
+        const wrapEl = document.createElement("div");
+        wrapEl.className = BEST_PRICE_CLASS_NAME;
+        if (!titleInfo) return wrapEl;
+        for (const u of titleInfo.units) {
+            const el = document.createElement("p");
+            el.innerHTML = u.price_display;
+            wrapEl.appendChild(el);
+        }
+        if (titleInfo.quantity_price_display) {
+            const qtyEl = document.createElement("p");
+            qtyEl.innerHTML = titleInfo.quantity_price_display;
+            wrapEl.appendChild(qtyEl);
+        }
+        if (wrapEl.childNodes.length) {
+            wrapEl.style.border = "1px solid red";
+            wrapEl.style.padding = "5px";
+            wrapEl.style.margin = "5px";
+            wrapEl.style.width = "fit-content";
+            for (const [k, v] of entries(extraStyle || {})) if (typeof v == "string") wrapEl.style[k] = v;
+        }
+        return wrapEl;
     }
     function processProductCard(cardEl, options) {
         var _a, _b, _c;
