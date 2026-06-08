73c1b78dc6f4262614865323bc922aa84634fdbc
apkawa <234165+apkawa@users.no-reply.gitverse.ru>
2026-06-08 11:10:29 +0000

    Deploying to release from @ apkawa/best_price_userscript@5612b62875aaeb295bf2aed27e07323c60952b6f 🚀

--- release/best_price/best_price.user.js
+++ release/best_price/best_price.user.js
@@ -19,6 +19,7 @@
 // @supportURL   https://github.com/Apkawa/best_price_userscript/issues
 // @downloadURL  https://gitverse.ru/api/repos/apkawa/best_price_userscript/raw/branch/release/release/best_price.user.js
 // @updateURL    https://gitverse.ru/api/repos/apkawa/best_price_userscript/raw/branch/release/release/best_price.user.js
+// @version      0.5.20
 // ==/UserScript==
 // src/utils/dom.ts
 function getElementByXpath(xpath, root = document) {
